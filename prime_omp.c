df3dd4f3474540dd6cc0eb22c9c8cd61c45b5d1c
ryoheitakayama <ryohei.takayama@gmail.com>
2015-09-13 04:37:23 +0900

    openMP var.を追加

---
+++ prime_omp.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<math.h>
+#include<omp.h>
+
+int main (void){
+  int i; //for scanf
+  int isPrime; //for HANTEI of prime
+  int count = 3;  //for numbering of prime
+  int n = 5;  //for natural numbers which are checked
+  int j;  //for checking whether 'n' is the prime
+  int a;
+
+  printf("How many prime numbers are there before the prime you want?\n"); 
+  scanf("%d",&i);
+
+  if(i == 0)
+    printf("the first prime is 2");
+  else if(i == 1)
+    printf("the second prime is 3");
+  else
+    while(count <= i+1){
+      isPrime = 1;
+      a = sqrt(n);
+      #pragma omp parallel
+      {
+      #pragma omp for
+        for(j = 3; j < a+1; j+=2){
+          if(n % j == 0){
+            isPrime = 0;
+          }
+        }
+      }
+
+      if(isPrime){
+        if(count == i+1){
+          if(count % 10 == 1)
+            printf("the %dst prime is %d.\n",count,n);
+          else if(count % 10 == 2)
+            printf("the %dnd prime is %d.\n",count,n);
+          else if(count % 10 == 3)
+            printf("the %drd prime is %d.\n",count,n);
+          else
+            printf("the %dth prime is %d.\n",count,n);
+        }
+        count = count + 1;
+      }
+      n = n + 2;
+    }
+}
